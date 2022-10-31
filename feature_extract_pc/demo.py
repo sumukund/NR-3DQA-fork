7bb9d743479b0f62bb4f078002787a37b934b4e9
sumukund <mukund.su@northeastern.edu>
2022-10-31 21:29:36 +0000

    why wont this work

--- feature_extract_pc/demo.py
+++ feature_extract_pc/demo.py
@@ -2,17 +2,28 @@ from feature_extract import get_feature_vector
 import time
 
 #demo
-objpath = "models/hhi_5.ply"
-start = time.time()
-features = get_feature_vector(objpath)
-end = time.time()
-time_cost = end-start
+def main(config):
+    start = time.time()
+    features = get_feature_vector(config.objpath)
+    end = time.time()
+    time_cost = end-start
 
 
-#show the features
-cnt = 0
-for feature_domain in ['l','a','b','curvature','anisotropy','linearity','planarity','sphericity']:
-    for param in ["mean","std","entropy","ggd1","ggd2","aggd1","aggd2","aggd3","aggd4","gamma1","gamma2"]:
-        print(feature_domain + "_" + param + ": " + str(features[cnt]))
-        cnt = cnt + 1
-print("Cost " + str(time_cost) + " sec.")
+    #show the features
+    cnt = 0
+    for feature_domain in ['l','a','b','curvature','anisotropy','linearity','planarity','sphericity']:
+        for param in ["mean","std","entropy","ggd1","ggd2","aggd1","aggd2","aggd3","aggd4","gamma1","gamma2"]:
+            print(feature_domain + "_" + param + ": " + str(features[cnt]))
+            cnt = cnt + 1
+    print("Cost " + str(time_cost) + " sec.")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    # input parameters
+    parser.add_argument('--objpath', type=str)
+
+    config = parser.parse_args()
+
+    main(config)
